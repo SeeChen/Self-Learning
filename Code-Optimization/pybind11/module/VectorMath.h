8741aaae803313a8c57158a03e54f66854467dbb
SeeChen <leeseechen@gmail.com>
2025-06-13 07:03:36 +0800

    [Issue https://github.com/SeeChen/Self-Learning/issues/5] Vector2D 1. Implement algorithm for Vecotr in Vector2D.cpp

--- Code-Optimization/pybind11/module/VectorMath.h
+++ Code-Optimization/pybind11/module/VectorMath.h
@@ -3,15 +3,35 @@
 
 #include <string>
 
+// Class of Vertor 2D
 struct Vector2D {
 
     double x, y;
 
+    // Constructor
     Vector2D(
         double x = 0,
         double y = 0
     );
+
+    double magnitude () const;
+
+    // Vector Addition
+    Vector2D operator + (
+        const Vector2D& other
+    ) const;
+
+    // Scalar multiplication
+    Vector2D operator * (
+        double factor
+    ) const;
+
+    std::string toString() const;
 };
 
+Vector2D operator * (
+    double factor,
+    const Vector2D& vec
+);
 
 #endif
--- Code-Optimization/pybind11/module/Vertor2D.cpp
+++ Code-Optimization/pybind11/module/Vertor2D.cpp
@@ -10,5 +10,39 @@ Vector2D::Vector2D(
     x(x), y(y) 
 {}
 
+double Vector2D::magnitude() const {
+    return std::sqrt(
+        x * x + y * y
+    );
+}
 
+Vector2D Vector2D::operator + (const Vector2D& other) const {
+    return Vector2D(
+        x + other.x,
+        y + other.y
+    );
+}
+
+Vector2D Vector2D::operator * (double factor) const {
+    return Vector2D(
+        x * factor,
+        y * factor
+    );
+}
+
+std::string Vector2D::toString() const {
+    std::stringstream stream;
+    stream << "Vector2D(" << x << ", " << y << ")";
+    return stream.str();
+}
+
+Vector2D operator * (
+    double factor,
+    const Vector2D& vec
+) {
+    return Vector2D(
+        vec.x * factor,
+        vec.y * factor
+    );
+}
 
