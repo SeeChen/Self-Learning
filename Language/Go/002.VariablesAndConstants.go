3896389704887cbc194d0ac0ab8e774f320d1d46
SeeChen <leeseechen@gmail.com>
2025-07-24 22:57:14 +0800

    [Issue https://github.com/SeeChen/Self-Learning/issues/7] Auto determine and := Symbol 1. Try to learn and understand Golang's function of automatically determining variable types. 2. Learn how to use the := symbol to assign values to variables.

--- Language/Go/002.VariablesAndConstants.go
+++ Language/Go/002.VariablesAndConstants.go
@@ -3,10 +3,12 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
 	
+	// #1
 	// Variables can be defined using the "var identifier type".
 	var SeeChen string = "SeeChen";
 
@@ -15,4 +17,32 @@ func main() {
 
 	fmt.Printf("My name is %s.  \n", SeeChen  );
 	fmt.Printf("My name is %s%s.\n", See, Chen);
+
+	// #2
+	// Not only can define variables with a specified type
+	// Can also let golang automatically determine the type of the variable based on the value of the variable.
+	var varBool = true     ;
+	var varStr  = "SeeChen";
+	var varInt  = 2000     ;
+
+	fmt.Println();
+	fmt.Println(reflect.TypeOf(varBool).String());
+	fmt.Println(reflect.TypeOf(varStr ).String());
+	fmt.Println(reflect.TypeOf(varInt ).String());
+	// When using the above method, must assign a value to the variable.
+	// If only use "var identifier", an error will be reported during compilation.
+
+	// #3
+	// := symbol.
+	// When assigning a value to an identifier, be sure not to use "var identifier"
+	// Otherwise an error will be reported.
+	intValue := 1;
+	strValue := "String";
+
+	fmt.Println();
+	fmt.Printf("%d, %s.\n", intValue, strValue);
+
+	// #4
+	// Multiple Assignments
+	
 }
