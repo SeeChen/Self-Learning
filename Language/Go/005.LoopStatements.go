40921a9db819d1ebbe5d8fce5119b0224b523504
SeeChen <leeseechen@gmail.com>
2025-07-31 22:53:54 +0800

    [Issue https://github.com/SeeChen/Self-Learning/issues/10] for loop statemtns

--- Language/Go/005.LoopStatements.go
+++ Language/Go/005.LoopStatements.go
@@ -23,4 +23,38 @@ func main() {
 		sum += 1;
 	}
 	fmt.Println(sum);
+
+	// 3. for-each range
+	// The for-each statement allows us to loop over strings, arrays, maps, etc.
+	strings := []string{"Lee", "See", "Chen"};
+	for i, s := range strings {
+		fmt.Printf("%d. %s\n", i, s);
+	}
+
+	// Alternatively, we can skip value to loop over.
+	// There are 3 ways to loop over a maps variables.
+	var myName map[string]string = map[string]string {
+		"FirstName" : "SeeChen",
+		"FamilyName": "Lee",
+	};
+
+	// Loop over the map with key and value
+	for key, value := range myName {
+		fmt.Printf("My %s is %s. ", key, value);
+	}
+	fmt.Println();
+
+	// Loop over without the value
+	for key := range myName {
+		fmt.Printf("My %s is %s. ", key, myName[key]);
+	}
+	fmt.Println();
+
+	// Loop over without the key.
+	// This method is similar to the key, value method.
+	// However, if use "_" in Golang, will get an error -- "cannot use _ as a value or type".
+	for _, value := range myName {
+		fmt.Printf("%s ", value);
+	}
+	fmt.Println();
 }
