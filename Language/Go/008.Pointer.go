c08dd0403df3a0fd3e674660c5da43686002a001
SeeChen <leeseechen@gmail.com>
2025-08-07 22:38:47 +0800

    [Issue https://github.com/SeeChen/Self-Learning/issues/13] Pointer to an Array 1. Using Explicit dereference 2. Modify original array without return value

--- Language/Go/008.Pointer.go
+++ Language/Go/008.Pointer.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 )
 
+func modifyArray(arrPtr *[10]int) {
+	// Modify an element via pointer to an array
+	(*arrPtr)[5] = 100;
+}
+
 func main() {
 	// The pointer points to the address of a variable.
 	// In Golang, use the "&" symbol before a parameter to get th address of a variable.
@@ -41,12 +46,12 @@ func main() {
 		fmt.Println("P is a null Porinter.");
 	}
 
-	// The array pointer
+	// Array of pointers
 	// In Golang, to define a pointer array, use var ptr [size]* type
 	var arr = [10]int{1, 3, 5, 7, 9, 2, 4, 6, 8, 10};	// Declare an array
 	var arrPtr1 [10]*int;
 
-	// Use a loop to assign each index
+	// Assign each elemet's address to the pointer array
 	for i := 0; i < len(arr); i++ {
 		arrPtr1[i] = &arr[i];
 	}
@@ -54,5 +59,25 @@ func main() {
 	for i := 0; i< len(arr); i++ {
 		fmt.Printf("%d, ", *arrPtr1[i]);
 	}
-	fmt.Println();
+	fmt.Println("\n");
+
+	// Pointer to an array
+	// Use var ptr *[size] type declaration
+	arr2 := [10]int {2, 4, 6, 8, 10, 1, 3, 5, 7, 9};
+	fmt.Println(arr2);
+
+	// Defined a pointer arrays
+	var ptrArr2 *[10]int;
+	ptrArr2 = &arr2;
+	fmt.Println(ptrArr2);
+
+	// Access array elements through the pointer
+	fmt.Println((*ptrArr2)[0]);	// Using explicit dereference
+	fmt.Println(ptrArr2[0]); 	// Shorthand (Go allow this)
+	fmt.Println(*ptrArr2);		// Get the entire array
+
+	// Use pointer to array in function
+	// Can modify original array without return value.
+	modifyArray(ptrArr2);
+	fmt.Println(arr2);
 }
